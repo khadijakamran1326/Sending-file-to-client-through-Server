005938aa7e1322a96a559404e74b4a9d5a386c0e
khadijakamran1326 <khadijakamran746@gmail.com>
2025-10-16 13:01:30 +0500

    Add files via upload

---
+++ Client Side (1).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+class FileClient
+{
+    static void Main()
+    {
+        var c = new TcpClient("10.10.20.4", 5000);
+        var ns = c.GetStream();
+        Console.Write("Enter file name:");
+        string name = Console.ReadLine();
+        ns.Write(Encoding.UTF8.GetBytes(name));
+        byte[] buf = new byte[1024];
+        using var fs = new FileStream("Received_" + name, FileMode.Create);
+        int bytes;
+        while ((bytes = ns.Read(buf))  > 0) fs.Write(buf, 0, bytes);
+        Console.WriteLine($"File saved as Received_{name}");
+        ns.Close();
+        c.Close();
+    }
+}
---
+++ Server side.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+class FileServer
+{
+    static void Main()
+
+    {
+        var server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+        Console.WriteLine("Serever ready..........");
+        while (true)
+        {
+            using var client = server.AcceptTcpClient();
+            using var ns = client.GetStream();
+            byte[] nameBuf = new byte[1024];
+            int len = ns.Read(nameBuf);
+            string file = Encoding.UTF8.GetString(nameBuf, 0, len);
+            if (File.Exists(file)) {
+                byte[] data = File.ReadAllBytes(file);
+                ns.Write(data);
+                Console.WriteLine($"SENT:{file}");
+        }
+            else {
+                byte[] msg = Encoding.UTF8.GetBytes("FILE NOT FOUND");
+                ns.Write(msg);
+                Console.WriteLine($"Missing:{file}");
+            } }
+    }
+}
